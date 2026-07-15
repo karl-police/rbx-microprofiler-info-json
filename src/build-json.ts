23b4af0231d1f63f80d9919b849e2e1131ea0117
karl-police <karl-police2001@bluewin.ch>
2026-07-15 21:43:05 +0200

    Update build-json.ts

--- src/build-json.ts
+++ src/build-json.ts
@@ -1,5 +1,12 @@
+
+// Do NOT remove content above
+
 import fs from "fs"
 
 let dataJson = ["test"]
+
+
+
+// Do NOT remove content below
 
 fs.writeFileSync( "output.json", JSON.stringify(dataJson) );
