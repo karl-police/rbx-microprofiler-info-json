4f33748d893dfe52f077a6da7fc7b8b03c2fc588
karl-police <karl-police2001@bluewin.ch>
2026-07-16 00:37:46 +0200

    Update build-json.ts

--- src/build-json.ts
+++ src/build-json.ts
@@ -1,12 +1,16 @@
+import TagInfoBuilder from "./builderOut.js"
 
+//
 // Do NOT remove content above
+//
 
 import fs from "fs"
 
 let dataJson = ["test"]
 
 
-
+//
 // Do NOT remove content below
+//
 
 fs.writeFileSync( "output.json", JSON.stringify(dataJson) );
