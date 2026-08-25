718bcacd41d52cac5909634a6a4535f7ae2af4aa
karl-police <karl-police2001@bluewin.ch>
2026-08-25 18:07:26 +0200

    Update build-json.ts

--- src/build-json.ts
+++ src/build-json.ts
@@ -6,13 +6,41 @@ import fs from "fs"
 // Do NOT remove content above
 //
 
+const TagTokenEntry = TagDataClasses.TagTokenEntry
+const TagGroupEntry = TagDataClasses.TagGroupEntry
+const TagLabel = TagDataClasses.TagLabel
+
 
 let builder = new TagDataBuilder()
 
-builder.AddTagToken(
-    new TagDataClasses.TagTokenEntry("test")
-        .SetDescription("test")
-        .WithColor([255,0,0])
+builder.AddTagGroup(
+    new TagGroupEntry("LuaBridge")
+        .SetDescription(`
+            This is LuaBridge!
+            Very cool!
+        `)
+)
+builder.AddTagGroup(
+    new TagGroupEntry("Systems")
+        .SetDescription(`
+            This is Systems
+            Hmmmm
+        `)
+)
+
+
+builder.AddGenericTagEntries(
+    [
+        new TagTokenEntry("test")
+            .SetDescription("This is test")
+            .WithColor([255,0,0]),
+        new TagTokenEntry("test2")
+            .SetDescription("This is test2")
+            .WithColor([0,255,0]), 
+        new TagTokenEntry("test3")
+            .SetDescription("This is test3")
+            .WithColor([0,0,255]), 
+    ]
 )
 
 builder.Finalize()
